d1a64e8cd2044be9fc30e0381709203fde82da91
cwj57740 <chenweiji57740@gmail.com>
2018-01-05 17:37:30 +0800

    跨域修正

--- svm/settings.py
+++ svm/settings.py
@@ -102,4 +102,32 @@ USE_L10N = True
 
 STATIC_URL = '/static/'
 
+CORS_ALLOW_CREDENTIALS = True
 CORS_ORIGIN_ALLOW_ALL = True
+CORS_ORIGIN_WHITELIST = (
+    '*'
+)
+
+CORS_ALLOW_METHODS = (
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+    'VIEW',
+)
+
+CORS_ALLOW_HEADERS = (
+    'XMLHttpRequest',
+    'X_FILENAME',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+    'Pragma',
+)
